82bfacd287bcdcb57e60862ec912aadc60e4830f
Catalin-Andronie <catalin.andronie@gmail.com>
2023-11-07 20:42:31 +0200

    - PASS: `GetWeatherForecastQuery_Should_RequireMinimumFields`

--- src/Web/Application/UserCases/Forecast/GetWeatherForecast/GetWeatherForecastQueryHandler.cs
+++ src/Web/Application/UserCases/Forecast/GetWeatherForecast/GetWeatherForecastQueryHandler.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
+using DbmlNet.Web.Application.Common;
+
 namespace DbmlNet.Web.Application.UserCases.Forecast.GetWeatherForecast;
+
+#pragma warning disable CA1822 // Mark members as static
 
 public sealed class GetWeatherForecastQueryHandler
 {
@@ -17,6 +21,31 @@ public sealed class GetWeatherForecastQueryHandler
         GetWeatherForecastQuery command,
         CancellationToken cancellationToken = default)
     {
+        await ValidateAsync(command).ConfigureAwait(false);
+
         throw new NotImplementedException();
+    }
+
+    private static Task ValidateAsync(GetWeatherForecastQuery command)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+
+        List<ValidationError> failures = new List<ValidationError>();
+
+        if (command.NumberOfDays < 1)
+        {
+            failures.Add(new ValidationError
+            {
+                PropertyName = nameof(command.NumberOfDays),
+                ErrorMessage = "The number of days cannot be less than 1."
+            });
+        }
+
+        if (failures.Count != 0)
+        {
+            throw new ApplicationValidationException(failures);
+        }
+
+        return Task.CompletedTask;
     }
 }
