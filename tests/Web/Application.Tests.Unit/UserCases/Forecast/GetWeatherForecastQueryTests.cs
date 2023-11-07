4342bb79b8694c096234da185b37f3ed935d503c
Catalin-Andronie <catalin.andronie@gmail.com>
2023-11-07 20:43:50 +0200

    - FAIL: `GetWeatherForecastQuery_Should_ReturnExpectedForecast`

--- tests/Web/Application.Tests.Unit/UserCases/Forecast/GetWeatherForecastQueryTests.cs
+++ tests/Web/Application.Tests.Unit/UserCases/Forecast/GetWeatherForecastQueryTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -43,5 +44,26 @@ public sealed class GetWeatherForecastQueryTests
         Assert.Equal("NumberOfDays", error.Key);
         string errorMessage = Assert.Single(error.Value);
         Assert.Equal("The number of days cannot be less than 1.", errorMessage);
+    }
+
+    [Fact]
+    public async Task GetWeatherForecastQuery_Should_ReturnExpectedForecast()
+    {
+        int numberOfDays = DataGenerator.GetRandomNumber(min: 1, max: 20);
+        GetWeatherForecastQuery query = new GetWeatherForecastQuery(numberOfDays);
+        GetWeatherForecastQueryHandler handler = new GetWeatherForecastQueryHandler();
+
+        WeatherForecast[] forecasts =
+            (await handler.HandleAsync(query).ConfigureAwait(true)).ToArray();
+
+        Assert.NotEmpty(forecasts);
+        Assert.True(numberOfDays == forecasts.Length, $"Expected number of forecasts ({forecasts.Length}) to match the requested number of days ({numberOfDays}).");
+        for (int i = 0; i < forecasts.Length; i++)
+        {
+            WeatherForecast forecast = forecasts[i];
+            Assert.Equal(DateOnly.FromDateTime(DateTime.Now.AddDays(i + 1)), forecast.Date);
+            Assert.NotNull(forecast.Summary);
+            Assert.Equal(forecast.TemperatureF, 32 + (int)(forecast.TemperatureC / 0.5556));
+        }
     }
 }
